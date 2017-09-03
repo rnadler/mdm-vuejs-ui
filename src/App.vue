4743cdfd19d920d3eba13ef9275b4e58dcb574fa
rnadler <robert.nadler@gmail.com>
2017-09-02 17:24:45 -0700

    Initial import

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import axios from 'axios'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  },
+  data: () => ({
+    controls: []
+  }),
+  created() {
+    axios.get(`/static/mini_model_ui_example.json`)
+      .then(response => {
+        this.controls = response.data.Controls;
+      })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
